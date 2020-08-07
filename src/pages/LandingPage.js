6c224eccc60c411849b1e6c7e5e268f325d4b083
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-08-08 03:46:30 +0700

    add testimonial and categories section

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from 'parts/MostPicked';
+import Categories from 'parts/Categories';
+import Testimony from 'parts/Testimony';
 import landingPage from "json/landingPage.json";
 export default class LandingPage extends Component {
   constructor(props){
@@ -20,6 +22,10 @@ export default class LandingPage extends Component {
           refMostPicked={this.refMostPicked} 
           data={landingPage.mostPicked}
         />
+        <Categories
+          data={landingPage.categories}
+        />
+        <Testimony data={landingPage.testimonial}/>
       </>
     )
   }
