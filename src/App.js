997cee1312ab368c2e8594992daa9684600a4bf1
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-08-07 14:34:12 +0700

    add image and add MostPicked component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import 'assets/scss/style.scss';
 import LandingPage from "pages/LandingPage";
 
